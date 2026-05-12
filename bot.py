e9e5ef54e537b04d3e91fcc2de8736d49177b2a5
Psycho-Motive <baodex69@gmail.com>
2026-05-12 19:52:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from telegram.ext import (
 )
 
 # ─────────────────────────────────────────
-TOKEN = os.environ.get(", "")
+TOKEN = os.environ.get("BOT_TOKEN", "")
 DB_PATH = "diary.db"
 DEFAULT_TZ = "Europe/Moscow"
 SUMMARY_HOUR = 21   # во сколько слать вечернюю сводку
